3319d8bb57ba2560d39a6cc6fd372ddfc83cecc3
Nicolas Charlery <nicolas@bokay.io>
2018-05-11 15:29:36 -0400

    Extract version number in module variable

--- mix.exs
+++ mix.exs
@@ -1,10 +1,12 @@
 defmodule FileStreamer.MixProject do
   use Mix.Project
 
+  @version "0.1.0"
+
   def project do
     [
       app: :file_streamer,
-      version: "0.1.0",
+      version: @version,
       elixir: "~> 1.6",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
