6c08e645f43b6d60e6adc6602e5f6af739578397
Nicolas Charlery <nicolas@bokay.io>
2019-06-14 13:22:08 -0400

    chore: Bump version

--- mix.exs
+++ mix.exs
@@ -1,7 +1,7 @@
 defmodule RemoteFileStreamer.MixProject do
   use Mix.Project
 
-  @version "1.0.0"
+  @version "1.0.1"
   @project_url "https://github.com/civilcode/remote-file-streamer"
 
   def project do
